2003e3638433006aa43637acc81db8cfb6728316
intae kim <intaemon@intaeui-MacBookAir.local>
2021-06-05 12:50:46 +0900

    new: geolocation api,   Leaflet 라이브러리 사용하기

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const log = console.log;
+
+navigator.geolocation.getCurrentPosition(function ({coords: {latitude, longitude}}) {
+  log(`https://www.google.co.kr/maps/@${latitude},${longitude}`);
+  
+  const coords = [latitude, longitude];
+
+  const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+}, function () {
+  alert('Could not get your position');
+}) 
+
