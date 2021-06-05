a3e6a4ea7317774b41c4e7d9025985ea360fca85
intae kim <intaemon@intaeui-MacBookAir.local>
2021-06-05 13:51:09 +0900

    new: sumbit -> marker 생성

--- script.js
+++ script.js
@@ -10,33 +10,47 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
 const log = console.log;
+let map, mapEvent;
 
 navigator.geolocation.getCurrentPosition(function ({coords: {latitude, longitude}}) {
   log(`https://www.google.co.kr/maps/@${latitude},${longitude}`);
   
   const coords = [latitude, longitude];
 
-  const map = L.map('map').setView(coords, 13);
+  map = L.map('map').setView(coords, 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  map.on('click', function({latlng: {lat: latitude, lng: longitude}}) {
-    const coords = [latitude, longitude];
-    L.marker(coords).addTo(map)
-    .bindPopup(L.popup({
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup',
-    }))
-    .setPopupContent('work out!')
-    .openPopup();
+  // handling clicks on map
+  map.on('click', function(mapE) {
+    mapEvent = mapE;
+    form.classList.remove('hidden')
+    inputDistance.focus();
+
   })
 }, function () {
   alert('Could not get your position');
 }) 
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  // Clear Input Fields
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+  const {latlng: {lat: latitude, lng: longitude}} = mapEvent;
+  const coords = [latitude, longitude];
+  L.marker(coords).addTo(map)
+  .bindPopup(L.popup({
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup',
+  }))
+  .setPopupContent('work out!')
+  .openPopup();
+})
